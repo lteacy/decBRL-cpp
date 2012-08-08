39c9f3de6643ec34d4e321aa6477661931ddd9fc
Luke Teacy <wtlt@ecs.soton.ac.uk>
2012-08-08 13:18:05 +0100

    Added mainpage for API documentation

---
+++ include/mainpage.h
@@ -0,0 +1,23 @@
+/**
+ * @file mainpage.h
+ * This header defines the main page for this documentation, and is for that
+ * purpose only.
+ * @author Dr W. T. Luke Teacy, University of Southampton
+ */
+namespace dec_brl {
+/**
+ * @mainpage
+ * \tableofcontents
+ * @section intro Overview
+ * This library is under development, but will eventually provide an
+ * an C++ implementation of the Decentralised Bayesian RL algorithms described
+ * in http://eprints.soton.ac.uk/273201/
+ *
+ * The source code for this library can be dowloaded from
+ * <a href="https://github.com/lteacy/decBRL-cpp">here</a>.
+ *
+ * If you have any comments or suggestions about this library or its
+ * documentation, please let me know. My contact details can be found
+ * here: http://users.ecs.soton.ac.uk/wtlt/
+ */
+}
