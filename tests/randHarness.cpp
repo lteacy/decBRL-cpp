5e6eb61edaf2f4826b6d60c023b3bc3bb4a89104
Luke Teacy <wtlteacy@soton.ac.uk>
2013-02-22 11:01:27 +0000

    copied mdpHarness to randHarness to make way for new test.

---
+++ tests/randHarness.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "dec_brl/SimpleMDP.h"
+#include "dec_brl/RandomPolicy.h"
+#include "dec_brl/random.h"
+#include <algorithm>
+
+
+/**
+ * Test harness simulates simple MDP and tests planning strategies.
+ */
+int main()
+{
+   using namespace dec_brl;
+   double x = 2;
+   double y = 3;
+   std::cout << "Hello world! " << std::min<const double&>(x,y) << std::endl;
+
+   //***************************************************************************
+   // Initialse random number generator used by all stochastic functions in
+   // this library.
+   //***************************************************************************
+   random::initRandomEngineByTime();
+
+   //***************************************************************************
+   // Create MDP Simulator
+   //***************************************************************************
+   SimpleMDP mdp;
+
+   //***************************************************************************
+   // Create planner
+   //***************************************************************************
+   RandomPolicy<SimpleMDP::ActionDomainType,SimpleMDP::StateType>
+      policy(mdp.getActionDomain());
+
+   const SimpleMDP::ActionDomainType& actionDomain
+      = mdp.getActionDomain();
+
+   std::cout << "Action Domain: size " << actionDomain.size() << std::endl;
+
+   //***************************************************************************
+   // Simulate planner interacting with environment for a number of timesteps
+   //***************************************************************************
+   int timesteps = 10;
+   SimpleMDP::StateType state = mdp.initState();
+   for(int i=0; i<timesteps; i++)
+   {
+      SimpleMDP::ActionType action = policy.act(state);
+      SimpleMDP::RewardType reward;
+      mdp.simulate(action,state,reward);
+
+      std::cout << "timestep " << i << ": <a:" << action << " s:" << state
+                << " r: " << reward << ">" << std::endl;
+   }
+   
+   //***************************************************************************
+   // Return sucessfully
+   //***************************************************************************
+   return 0;
+}
+
