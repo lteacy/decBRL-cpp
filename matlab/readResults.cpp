7cc5fe67154e1abb9ad22556b28b13809022d669
Luke Teacy <wtlteacy@soton.ac.uk>
2013-04-21 20:26:27 +0100

    continued work on mex result file reader

--- matlab/readResults.cpp
+++ matlab/readResults.cpp
@@ -1,8 +1,37 @@
 #include <matrix.h>
 #include <mex.h>
 #include "dec_brl/ProtoReader.h"
+#include <string>
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-     mexPrintf("Hello World!\n");
-}
+    //**************************************************************************
+    //  Try to get filename from input arguments
+    //**************************************************************************
+    if(1>nrhs)
+    {
+        mexErrMsgIdAndTxt("READRESULT:ARGS", "Filename must be specified.");
+    }
+    
+    const mxArray* matFilename = prhs[0];
+    if(!mxIsChar(matFilename))
+    {
+        mexErrMsgIdAndTxt("READRESULT:ARGTYPE",
+                          "Filename argument must be char array.");
+    }
+    
+    const char* const psFilename = mxArrayToString(matFilename);
+    
+    if(NULL==psFilename)
+    {
+        mexErrMsgIdAndTxt("READRESULT:ARGTYPE:INTERNAL",
+                          "Internal error reading filename.");
+    }
+    
+    mexPrintf("Requested File: %s\n",psFilename);
+    
+    //**************************************************************************
+    //  Try to Parse file
+    //**************************************************************************
+
+} // mexFunction
